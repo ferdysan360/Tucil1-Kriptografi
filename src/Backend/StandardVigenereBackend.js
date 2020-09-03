922dd2a3c12b22e21c0ddcf5a50074b273a91266
unknown <saragih.meyer0821@gmail.com>
2020-09-03 18:51:49 +0700

    Revise backend

---
+++ src/Backend/StandardVigenereBackend.js
@@ -0,0 +1,78 @@
+/*---------------- CIPHERING FUNCTION ------------------*/
+/* Encoding */
+export function encode(plainText, key) {
+    let cipherText = "";
+    let keyCodes = getKeys(key);
+    plainText = cleanText(plainText).toLowerCase();
+
+    console.log(keyCodes);
+
+    for (let i = 0; i < plainText.length; i++) {
+        let charNum = plainText.charCodeAt(i) - 97;
+        let currentKey = keyCodes[i % keyCodes.length];
+
+        if (charNum >= 0 && charNum <= 25) {
+            charNum = (((charNum + currentKey) % 26) + 26) % 26;
+            charNum = charNum + 65;
+            cipherText += String.fromCharCode(charNum);
+        }
+    }
+    return cipherText;
+}
+
+/* Decoding */
+export function decode(cipherText, key) {
+    let plainText = "";
+    let keyCodes = getKeys(key);
+    cipherText = cleanText(cipherText).toUpperCase();
+
+    for (let i = 0; i < cipherText.length; i++) {
+        let charNum = cipherText.charCodeAt(i) - 65;
+        let currentKey = keyCodes[i % keyCodes.length];
+
+        if (charNum >= 0 && charNum <= 25) {
+            charNum = (((charNum - currentKey) % 26) + 26) % 26;
+            // console.log(charNum);
+            charNum = charNum + 65;
+            plainText += String.fromCharCode(charNum);
+        }
+    }
+    return plainText;
+}
+
+export function splitByFive(text) {
+    let result = "";
+
+    for (let i = 0; i < text.length; i++) {
+        if (i % 5 === 0 && i !== 0) {
+            result += " ";
+        }
+        result += text.charAt(i);
+    }
+    return result;
+}
+
+function cleanText(text) {
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+        if ((text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) || (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122)) {
+            result += text.charAt(i);
+        }
+    }
+    return result;
+}
+
+function getKeys(text) {
+    let result = [];
+    for (let i = 0; i < text.length; i++) {
+        let charNum = text.charCodeAt(i) - 97;
+        if (charNum < 0) {
+            charNum += 32;
+        }
+
+        if (charNum >= 0 && charNum <= 25) {
+            result.push(charNum);
+        }
+    }
+    return result;
+}
